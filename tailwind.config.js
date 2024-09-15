91bdb33f187141e87d320b0a9d6aed2cf1c2e0e0
Ananthapadmanabhan M <ananthapadmanabhanr2@gmail.com>
2024-09-15 06:55:20 +0530

    I have re Installed all the packages

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'main-font': '"Bebas Neue"',
+        'sub-font': '"Be Vietnam Pro"',
+      }
+    },
+  },
+  plugins: [],
+}
+
